32a56725b985a1db3ddafdc6f7d2bb69698faff3
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-08 13:33:06 +0100

    Created flip and choice funcitonality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import { render } from 'react-dom';
 import styles from './App.module.css';
 
-function Cards() {
-
+function Card(props) {
+  return (
+      <div id={props.id}>
+        <img src={props.src} alt="A memory card" className={props.flipped ? styles.cardImg : styles.imgHidden} onClick={props.handleCardChoice}/>
+        <div className={props.flipped ? styles.imgHidden : styles.cardFront} onClick={() => props.handleCardChoice(props.id)}></div>
+      </div>  
+    )
 }
 
 function App() {
@@ -13,10 +19,22 @@ function App() {
     {'src': '/RegalMoth.svg'},
     {'src': '/Rose.svg'}
   ];
-
+  
   const [cards, setCards] = React.useState([]); 
+  const [cardOne, setCardOne] = React.useState(null); 
+  const [cardTwo, setCardTwo] = React.useState(null); 
 
-  //Denna kommer nog bli en callback handler, ska skickas med props och sen anropas från Cards... 
+  React.useEffect(() => {
+    console.log("UseEffect triggades");
+  }, [cardOne, cardTwo]); 
+
+
+  const handleCardChoice = (id) => {
+    console.log(id);  
+    
+    cardOne ? setCardTwo(id) : setCardOne(id); 
+  }
+
   const initiateCardsArray = () => {
     const doubled = [...images, ...images]; 
     const shuffled = doubled.sort(() => Math.random() - 0.5);
@@ -24,19 +42,33 @@ function App() {
       return {...card, id: Math.random()}; 
     });
     setCards(memoryCards);
-
-    console.log(cards); 
+    console.log("initiateCardsArray ran, cards: ", cards); 
   }
 
-  
 
   return (
     <div className="App">
       <div className={styles.wrapper}>
         <button className={styles.button} onClick={initiateCardsArray}>New Game</button>
-        {/* <p>{cards[0]}</p> */}
+        {console.log(cards)}
+        <div className={styles.cardsWrapper}>
+          {cards.map(card => {
+            return(
+              <Card 
+                key={card.id}
+                handleCardChoice={handleCardChoice}
+                id={card.id}
+                src={card.src}
+                flipped={card.id === cardOne || card.id === cardTwo ? true : false}
+              />
+            )
+          })}
+        </div>
       </div>
+      {console.log("Card one: ", cardOne)} 
+      {console.log("Card two: ", cardTwo)}
     </div>
+    
   )
 }
 
